4ab3c238cab480e937c67f7e9048e87ee4afaf57
Kevin Tse <ktse@fb.com>
2021-07-16 14:42:10 -0700

    adding 'dx' version, extensive testing and documentation

--- aten/src/ATen/native/Integration.cpp
+++ aten/src/ATen/native/Integration.cpp
@@ -45,6 +45,13 @@ Tensor do_cumulative_trapezoid(const Tensor& y, const Tensor& dx, int64_t dim) {
     return ((left + right) * dx).cumsum(dim) / 2.;
 }
 
+Tensor do_cumulative_trapezoid(const Tensor& y, double dx, int64_t dim) {
+    Tensor left = y.slice(dim, 0, -1);
+    Tensor right = y.slice(dim, 1);
+
+    return (dx /2. * (left + right)).cumsum(dim);
+}
+
 }
 
 Tensor trapezoid(const Tensor& y, const Tensor& x, int64_t dim) {
@@ -91,11 +98,6 @@ Tensor trapz(const Tensor& y, double dx, int64_t dim) {
 
 Tensor cumulative_trapezoid(const Tensor& y, const Tensor& x, int64_t dim) {
     dim = maybe_wrap_dim(dim, y);
-    // asking for the integral with zero samples is a bit nonsensical,
-    // but we'll return "0" to match numpy behavior.
-    if (y.size(dim) == 0) {
-      return zeros_like_except(y, dim);
-    }
     TORCH_CHECK(y.scalar_type() != kBool && x.scalar_type() != kBool, "cumulative_trapezoid: received a bool input for `x` or `y`, but bool is not supported")
     Tensor x_viewed;
     if (x.dim() == 1) {
@@ -103,9 +105,9 @@ Tensor cumulative_trapezoid(const Tensor& y, const Tensor& x, int64_t dim) {
         DimVector sizes(y.dim(), 1); // shape = [1] * y.
         sizes[dim] = x.size(0); // shape[axis] = d.shape[0]
         x_viewed = x.view(sizes);
-        } else {
-            x_viewed = x;
-        }
+    } else {
+        x_viewed = x;
+    }
     Tensor x_left = x_viewed.slice(dim, 0, -1);
     Tensor x_right = x_viewed.slice(dim, 1);
     Tensor dx = x_right - x_left;
@@ -114,13 +116,10 @@ Tensor cumulative_trapezoid(const Tensor& y, const Tensor& x, int64_t dim) {
 }
 
 Tensor cumulative_trapezoid(const Tensor& y, const Scalar& dx, int64_t dim) {
-    if (y.size(dim) == 0) {
-      return zeros_like_except(y, dim);
-    }
     TORCH_CHECK(y.scalar_type() != kBool, "cumulative_trapezoid: received a bool input for `y`, but bool is not supported")
     TORCH_CHECK(!(dx.isComplex() or dx.isBoolean()), "trapezoid: Currently, we only support dx as a real number.");
-    Tensor d = at::range(0, y.size(dim), dx);
-    return do_cumulative_trapezoid(y, d, dim);
+
+    return do_cumulative_trapezoid(y, dx.toDouble(), dim);
 }
 
 }} // namespace at::native
--- docs/source/torch.rst
+++ docs/source/torch.rst
@@ -571,6 +571,7 @@ BLAS and LAPACK Operations
     lobpcg
     trapz
     trapezoid
+    cumulative_trapezoid
     triangular_solve
     vdot
 
