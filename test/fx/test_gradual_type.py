06919b4ffd4a83281b27ec2179e1b1677b484343
Zeina Migeed <migeedz@fb.com>
2021-07-19 18:12:47 -0700

    Refinement types and unification for symbolic shape inference
    ghstack-source-id: de29c292899fab84294024a105568297d4c5714c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61776

--- test/fx/test_gradual_type.py
+++ test/fx/test_gradual_type.py
@@ -1,6 +1,7 @@
 import unittest
 import torch
 from torch.fx import symbolic_trace
+from torch.fx.experimental.unify_refinements import infer_symbolic_types
 from torch.fx.tensor_type import TensorType, Dyn, is_consistent, is_more_precise
 from torch.fx.annotate import annotate
 from torch.fx.experimental.graph_gradual_typechecker import GraphTypeChecker, broadcast_types
@@ -15,11 +16,14 @@ try:
 except ImportError:
     HAS_TORCHVISION = False
 skipIfNoTorchVision = unittest.skipIf(not HAS_TORCHVISION, "no torchvision")
-skipIfNoMkldnn = unittest.skipIf(
-    not (torch.backends.mkldnn.enabled and torch.backends.mkldnn.is_available()),
-    "no MKLDNN",
-)
 
+try:
+    import unification
+    unification
+    HAS_UNIFICATION = True
+except ImportError:
+    HAS_UNIFICATION = False
+skipIfNoUnification = unittest.skipIf(not HAS_UNIFICATION, "no unification")
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
@@ -788,6 +792,7 @@ class TypeCheckerTest(unittest.TestCase):
                     assert is_consistent(n.type, TensorType(b.size()))
 
     @skipIfNoTorchVision
+    @skipIfNoUnification
     def test_resnet50(self):
         gm_run = symbolic_trace(resnet50())
         sample_input = torch.randn(1, 3, 224, 224)
@@ -818,6 +823,50 @@ class TypeCheckerTest(unittest.TestCase):
         g.type_check()
         for n1, n2 in zip(gm_static_with_types.graph.nodes, gm_run.graph.nodes):
             assert n1.type == TensorType(n2.meta['tensor_meta'].shape)
+
+        # apply shape inference to graph and check
+        # that the batch size is equal across all layers
+        infer_symbolic_types(gm_static)
+        batch_sizes = []
+        for n in gm_static.graph.nodes:
+            assert isinstance(n.type, TensorType)
+            batch_sizes.append(n.type.__args__[0])
+        assert (len(set(batch_sizes)) == 1)
+
+
+    @skipIfNoUnification
+    def test_type_check_batch_norm_symbolic(self):
+        class BasicBlock(torch.nn.Module):
+
+            def __init__(self, inplanes, planes, norm_layer=None):
+                super(BasicBlock, self).__init__()
+                if norm_layer is None:
+                    norm_layer = torch.nn.BatchNorm2d
+                self.bn1 = norm_layer(planes)
+
+            def forward(self, x: Dyn):
+                identity = x
+                out: TensorType((2, 2, Dyn, 4)) = self.bn1(x)
+                out += identity
+                return out
+
+        B = BasicBlock(2, 2)
+        ast_rewriter = RewritingTracer()
+        graph = ast_rewriter.trace(B)
+        traced = GraphModule(ast_rewriter.root, graph, "gm")
+        tc = GraphTypeChecker({}, traced)
+        tc.type_check()
+
+        infer_symbolic_types(traced)
+
+        my_types = []
+
+        for n in graph.nodes:
+            my_types.append(n.type)
+
+        assert my_types[0] == my_types[1]
+        assert my_types[1] == my_types[2]
+        assert my_types[2] == my_types[3]
 
 
 if __name__ == '__main__':
---
+++ torch/fx/experimental/unify_refinements.py
@@ -0,0 +1,86 @@
+from torch.fx.experimental.graph_gradual_typechecker import Refine
+from torch.fx.tensor_type import TensorType
+from copy import deepcopy
+
+try:
+    from unification import unify, Var  # type: ignore[import]
+except ImportError:
+    pass
+
+def infer_symbolic_types(traced):
+    """
+    Generate constraints over types,
+    solve constraints with unification,
+    apply solution back to the types
+    """
+    r = Refine(traced)
+    r.refine()
+    mgu = unify_eq(r.constraints)
+    substitute_all_types(traced.graph, mgu)
+
+def convert_eq(list_of_eq):
+    """
+    Convert equality constraints in the right format
+    to be used by unification library.
+    """
+    lhs = []
+    rhs = []
+    for eq in list_of_eq:
+        lhs.append(eq.lhs)
+        rhs.append(eq.rhs)
+    return tuple(lhs), tuple(rhs)
+
+
+def unify_eq(list_of_eq):
+    """
+    Apply unification to a set of
+    equality constraints
+    """
+    lhs, rhs = convert_eq(list_of_eq)
+    return unify(lhs, rhs)
+
+
+def substitute_solution_one_type(mapping, t):
+    """
+    Apply the most general unifier to a type
+    """
+    if isinstance(t, Var):
+        if t in mapping.keys():
+            return mapping[t]
+
+    elif isinstance(t, TensorType):
+        new_type = []
+        for typ in t.__args__:
+            if typ in mapping.keys():
+                new_type.append(mapping[typ])
+            else:
+                new_type.append(typ)
+        return TensorType(tuple(new_type))
+
+
+def substitute_all_types(graph, mapping):
+    """
+    Apply the most general unifier to all types in a graph
+    till reaching a fixed point. If the input and output graph
+    are the same, we converge.
+    """
+    old_graph = deepcopy(graph)
+    while True:
+        for n in graph.nodes:
+            n.type = substitute_solution_one_type(mapping, n.type)
+        if check_for_type_equality(old_graph, graph):
+            break
+        else:
+            old_graph = deepcopy(graph)
+
+
+def check_for_type_equality(g1, g2):
+    """
+    A check equality to be used in fixed points.
+    We do not use graph equality but instead type
+    equality.
+    """
+    for n, m in zip(g1.nodes, g2.nodes):
+        if n.type != m.type:
+            return False
+    return True
